d528414ec378a1a204f335ebca6a74aa9fa78e03
Mayke Erick <maykeesa.contato@gmail.com>
2024-08-28 19:42:32 -0300

    feat: Reserva controller finalizado

---
+++ src/main/kotlin/br/com/sporthub/reserva/ReservaService.kt
@@ -0,0 +1,8 @@
+package br.com.sporthub.reserva
+
+import br.com.sporthub.service.GenericService
+import org.springframework.stereotype.Service
+
+@Service
+class ReservaService: GenericService<Reserva>(Reserva::class.java) {
+}
