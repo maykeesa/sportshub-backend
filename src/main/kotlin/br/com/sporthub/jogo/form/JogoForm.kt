1c1a36e2e62f0a997590d3cc6a07281ad7db2820
Mayke Erick <maykeesa.contato@gmail.com>
2024-09-03 12:20:27 -0300

    fix: Adição da mensagem no form do torneio

--- src/main/kotlin/br/com/sporthub/jogo/form/JogoForm.kt
+++ src/main/kotlin/br/com/sporthub/jogo/form/JogoForm.kt
@@ -3,8 +3,8 @@ package br.com.sporthub.jogo.form
 import br.com.sporthub.torneio.Torneio
 import jakarta.validation.constraints.NotBlank
 
-class JogoForm(
-    @NotBlank
+data class JogoForm(
+    @NotBlank(message = "O campo não pode ser vazio ou nulo.")
     var torneio: Torneio,
 ) {
 }
