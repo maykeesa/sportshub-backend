ee282dbf6f4d3f07c74883bb8888ab60fee307a0
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2024-09-24 15:58:08 -0300

    feat: criar rota get reserva por usuario

--- src/main/kotlin/br/com/sporthub/reserva/ReservaRepository.kt
+++ src/main/kotlin/br/com/sporthub/reserva/ReservaRepository.kt
@@ -12,4 +12,9 @@ interface ReservaRepository : JpaRepository<Reserva, UUID>{
 
     @Query("SELECT r FROM Reserva r WHERE r.horario.quadra.id = :quadraId AND r.dataReserva >= CURRENT_DATE")
     fun findReservasByQuadraId(@Param("quadraId") quadraId: UUID): List<Reserva>
+
+
+    //resevar por usuario
+    @Query("SELECT r FROM Reserva r WHERE r.usuario.id = :usuarioId ORDER BY r.dataReserva")
+    fun findReservasByUsuarioId(@Param("usuarioId") usuarioId: UUID): List<Reserva>
 }
